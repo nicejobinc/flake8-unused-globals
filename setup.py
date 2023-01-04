109dee5dcb98229cc45ab0a9a9e62ca18a964850
Nash Taylor <nash@nicejob.com>
2023-01-03 16:35:25 -0800

    bdist to sdist

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ with open("__VERSION__") as f:
 with open("requirements.txt") as f:
     install_requires = f.read().splitlines()
 
+with open("README.md") as f:
+    long_description = f.read().strip()
+
 setup(
     name="flake8-unused-globals",
     version=version,
@@ -14,4 +17,5 @@ setup(
     install_requires=install_requires,
     py_modules=["flake8_unused_globals"],
     entry_points={"flake8.extension": "UUG001 = flake8_unused_globals:Plugin"},
+    long_description=long_description,
 )
