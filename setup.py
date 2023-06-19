ca77b81cd51f5884d209914428fea28c992d24b2
Nash Taylor <nash@nicejob.com>
2023-06-19 15:09:06 -0700

    Allow Python 3.11

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md") as f:
 setup(
     name="flake8-unused-globals",
     version="0.1.9",
-    python_requires=">=3.10,<3.11",
+    python_requires=">=3.10",
     install_requires=install_requires,
     py_modules=["flake8_unused_globals"],
     entry_points={"flake8.extension": "UUG001 = flake8_unused_globals:Plugin"},
